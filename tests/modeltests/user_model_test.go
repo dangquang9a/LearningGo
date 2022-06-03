75f12299fbe05dbdc273b935f2e999696adfe457
Dang <merlin2508@icloud.com>
2022-06-03 17:44:40 +0700

    add tests

---
+++ tests/modeltests/user_model_test.go
@@ -0,0 +1,51 @@
+package modeltests
+
+import (
+	"log"
+	"testing"
+
+	"github.com/dangquang9a/LearningGo/api/models"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestFindAllUsers(t *testing.T) {
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = seedUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	users, err := userInstance.FindAllUsers(server.DB)
+	if err != nil {
+		t.Errorf("this is the error getting the users: %v\n", err)
+		return
+	}
+
+	assert.Equal(t, len(*users), 2)
+}
+
+func TestSaveUser(t *testing.T) {
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	newUser := models.User{
+		ID:       1,
+		Email:    "test@gmail.com",
+		Nickname: "test",
+		Password: "password",
+	}
+	savedUser, err := newUser.SaveUser(server.DB)
+	if err != nil {
+		t.Errorf("this is the error getting the users: %v\n", err)
+		return
+	}
+
+	assert.Equal(t, newUser.ID, savedUser.ID)
+	assert.Equal(t, newUser.Email, savedUser.Email)
+	assert.Equal(t, newUser.Nickname, savedUser.Nickname)
+}
